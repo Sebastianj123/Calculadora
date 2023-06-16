0258c1fd9e2cca7c3bff455cd2480e67043962e3
Sebastianj123 <113195211+Sebastianj123@users.noreply.github.com>
2023-06-15 20:34:24 -0500

    Calculadora
    Versión "final" de la calculadora.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
 // Principal
-const divAcumulado = document.getElementById('acumulado'),
+const as = document.querySelector('.a'),
+divAcumulado = document.getElementById('acumulado'),
 divNumeros = document.getElementById('numeros'),
-divOperador = document.querySelector('operador'),
 expresionOperadores = /\+|-|\*|\//gi,
 expresionNumeros = /\d/;
 
 var numeros = '',
-operadores = null,
-acumulador = null,
-resultado = null;
+acumulador = '',
+resultado = '',
+res = true;
 
 window.addEventListener('click',function(dates) {
 
-    
-    let filtro = dates.target.attributes.value.nodeValue,
-    operador = expresionOperadores.test(filtro);
-    // console.log(filtro);
-    // console.log(operador + " operador");
-    // console.log(expresionNumeros.test(filtro) + " numeros");
-    
-    switch (filtro) {
+    if (res) {
+            // Obtiene el valor del input (boton)
+    let valor = dates.target.attributes.value.nodeValue,
+    // Comprueba si el valor del input es un operador (+, -, *, /)
+    operador = expresionOperadores.test(valor);
+
+    // Habre el condicional con la variable valor
+    switch (valor) {
 
         // Recargar la página
         case 'CE':
@@ -30,67 +30,73 @@ window.addEventListener('click',function(dates) {
         // Borrar uno en uno
         case 'value':
 
-            if (numeros == null | numeros == '') {
+            if (numeros == null || numeros == '') {
+                
                 return this.alert('Ingrese un valor');
+
             } else {
+                
                 return numeros = numeros.substring(0, numeros.length -1), 
                 divNumeros.innerHTML = numeros;
             }
 
+        // Al dar igual
         case '=':
             
-            if (numeros == null | numeros == '') {
+            if (numeros == '' && acumulador == '') {
+            
                 return this.alert('Ingrese un valor');
-            } else {
-                return resultado = Math.floor(acumulador,operador,numeros);
-            }
-
-            // Números o operadores
-        default :
-        
-        if (operador) {
-
-            if (acumulador !=  null) {
-
-                console.log(233);
-
-                let operadorSeleccionado1 = filtro.match(expresionOperadores);
-                operadores = operadorSeleccionado1;
-                divOperador.innerHTML = operadores;
-                divNumeros.innerHTML = operadores + numeros;
-                break;                
-
-            } else {
-
-                console.log(4);
-                let operadorSeleccionado = filtro.match(expresionOperadores)
-                operadores = operadorSeleccionado;
-                acumulador = numeros;
-                numeros = '';
-                divAcumulado.innerHTML = acumulador;
-                divNumeros.innerHTML = operadores + numeros;
-                break;
-            }
-
-        } else {
             
-            if (operadores =! '') {
+            } else {
+
+                if (numeros == null || numeros == '') {
                 
-                console.log(1);
-                numeros = `${numeros}${filtro}`;
-                divNumeros.innerHTML = numeros;
-                divOperador = operadores;
-                break;
-            } 
-            
-            else {
+                    numeros = 0;
+                    acumulador += ` ${numeros}`;
 
-                console.log(12);
-                numeros = `${numeros}${filtro}`;
-                divNumeros.innerHTML = numeros;
+                } 
             
-            }   
+                acumulador += ` ${numeros}`
+                resultado = eval(acumulador);
+                
+                divNumeros.style.fontSize = '3vw';
+                divAcumulado.style.fontSize = '1.5vw';
+                return divAcumulado.innerHTML = acumulador,
+                divNumeros.innerHTML = resultado,
+                as.style.display = "block",
+                res = false,
+                alert('La calculadora está bloqueada, se reiniciara pronto (5s aproximadamente luego de este mensaje)'),
+                setTimeout(
+                    function() {
+                        location.reload()
+                    }, 5000);
+            }
 
-        }
+    
+            // Números o operadores    
+        default :
+
+            // Si es un operador
+            if (operador) {
+
+                // Seleccionamos el operador en caso de que sea un operador el selecto
+                let a = valor.match(expresionOperadores);
+                // acumulador = '';
+                acumulador += `${acumulador} ${numeros} ${a}`;
+                numeros = '';
+
+            } else {
+                
+                numeros += valor; 
+            }
+
+        break;
+    
     }
-})
+
+    divAcumulado.innerHTML = acumulador;
+    divNumeros.innerHTML = numeros;
+}
+    }
+
+)
